72f6137c05d425c0844507c0711afe8fe58c6b82
DhariniPanchall <97149398+DhariniPanchall@users.noreply.github.com>
2022-01-05 12:21:04 +0530

    Add files via upload

---
+++ String_dig_rev.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+	<body>
+		<?php
+			#Stringreverse
+			$str1 = "Hello";
+			echo "The String is: $str1";
+			echo "<br/>";
+			echo "The String's Reverse is: ".strrev($str1);
+			echo "<br/>";
+
+			#Digitreverse
+			$no = 12345;
+			echo "The Number is: $no";
+			echo "<br/>";
+			echo "The Number's Reverse is: ".strrev($no);
+		?>
+	</body>
+</html>
